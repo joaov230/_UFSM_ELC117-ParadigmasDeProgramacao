ad6f73d7de594d0a1e416615c2e168b0a602d091
João Vítor F. B <37089511+joaov230@users.noreply.github.com>
2018-04-11 16:52:09 -0300

    Add files via upload
    Ex 5 resolvido

--- t3/t3.pl
+++ t3/t3.pl
@@ -23,3 +23,14 @@ potN0(N,L) :-
     X is N-1,
     L = [R|B],
     potN0(X,B).
+
+
+% Ex 5
+
+zipmult([],[],[]).
+zipmult(L1,L2,L3) :-
+    L1 = [H1|T1],
+    L2 = [H2|T2],
+    H3 is H1*H2,
+    L3 = [H3|T3],
+    zipmult(T1,T2,T3).
