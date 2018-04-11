bcab77ceb5d0bb14071aaa8b085eb2424a7b2c83
João Vítor F. B <37089511+joaov230@users.noreply.github.com>
2018-04-11 17:31:40 -0300

    Add files via upload
    Ex 6 realizado.

--- t3/t3.pl
+++ t3/t3.pl
@@ -34,3 +34,18 @@ zipmult(L1,L2,L3) :-
     H3 is H1*H2,
     L3 = [H3|T3],
     zipmult(T1,T2,T3).
+
+
+% Ex 6
+
+potencias(N,L) :-
+  potenciasAux(N,L2),
+  reverse(L2,L).
+
+potenciasAux(0,[]).
+potenciasAux(N,L) :-
+  N2 is N-1,
+  N2 >= 0,
+  R is 2^N2,
+  L = [R|T],
+  potenciasAux(N2,T).
